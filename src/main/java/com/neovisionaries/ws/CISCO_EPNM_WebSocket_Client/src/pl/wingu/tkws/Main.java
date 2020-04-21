840579797c54682f24544a5b4fc5600065da0ef6
krzehan <krzesimir@hanebach.pl>
2020-04-21 23:28:41 +0200

    testing is.readLine() instead of is.readFrame() for output

--- src/main/java/com/neovisionaries/ws/CISCO_EPNM_WebSocket_Client/src/pl/wingu/tkws/Main.java
+++ src/main/java/com/neovisionaries/ws/CISCO_EPNM_WebSocket_Client/src/pl/wingu/tkws/Main.java
@@ -1,7 +1,6 @@
 package pl.wingu.tkws;
 
 import javax.net.ssl.*;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.NoSuchAlgorithmException;
 
@@ -10,7 +9,7 @@ public class Main {
 
     public static void main (String [] args){
 
-        String epnmHostname = "198.18.134.7";
+        String epnmHostname = args[0];
         WebSocketFactory factory = new WebSocketFactory();
 
         try {
@@ -26,13 +25,14 @@ public class Main {
         try {
             WebSocket ws = factory.createSocket("wss" +
                     "://" + epnmHostname + "//restconf//streams//v1//alarm.xml");
-            ws.setUserInfo("root", "Public123#");
+            ws.setUserInfo(args[1], args[2]);
             ws.connect();
 
             OutputStream os = ws.getOutput();
-            InputStream is = ws.getInput();
+            WebSocketInputStream is = ws.getInput();
             while(true){
-                System.out.println(((WebSocketInputStream) is).readFrame());
+
+                System.out.println(is.readLine());
             }
 
 
