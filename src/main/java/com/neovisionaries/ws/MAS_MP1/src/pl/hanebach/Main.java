00e4ed0d3b18e3a77e4220c1aa4d57b0ef6b7887
krzehan <krzesimir@hanebach.pl>
2021-05-06 00:14:07 +0200

    start menu

--- src/main/java/com/neovisionaries/ws/MAS_MP1/src/pl/hanebach/Main.java
+++ src/main/java/com/neovisionaries/ws/MAS_MP1/src/pl/hanebach/Main.java
@@ -4,6 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int option = 0;
+
+        switch(option){
+            case 0:
+                System.out.println("0 - List all dvcs");
+
+            case 1:
+                System.out.println("1 - List all dgrps");
+
+
+        }
 
 
 
